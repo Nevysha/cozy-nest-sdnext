075743b10c826bdfbe57cf9bcccad68329ca4950
Nevysha <me@nevysha.art>
2023-07-13 19:46:32 +0200

    Init

---
+++ src/refreshRuntime.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import RefreshRuntime from "/cozy-nest-dev/@react-refresh"
+RefreshRuntime.injectIntoGlobalHook(window)
+// @ts-ignore
+window.$RefreshReg$ = () => {}
+// @ts-ignore
+window.$RefreshSig$ = () => (type) => type
+// @ts-ignore
+window.__vite_plugin_react_preamble_installed__ = true
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // @ts-ignore
+  plugins: [react()],
+
+  server: {
+    host: '127.0.0.1'
+  },
+
+  base: '/cozy-nest-dev'
+})
